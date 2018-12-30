5385d4ae7c27da92c0ab9aa7392366293ffbb1d5
webmcki <webmcki@gmail.com>
2018-12-30 17:16:38 +0900

    함수형컴포넌트 정의

--- src/cpnts/propstest/child.js
+++ src/cpnts/propstest/child.js
@@ -1,20 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class child extends Component {
-  render() {
-    const {fromParents} = this.props
-    const fromParentsList = fromParents.map((fromParent) => {
-      return (
-        <div key={fromParent.id}>
-          <p>값을 넘겨 받자(자식)</p>
-          <div>이름: {fromParent.name}</div>
-          <div>나이: {fromParent.age}</div>
-          <div>벨트: {fromParent.belt}</div>
-          <div>-----------------------</div>
-        </div>
-      )
-    })
+const child = ({fromParents}) => {
+  const fromParentsList = fromParents.map((fromParent) => {
+    return (
+      <div key={fromParent.id}>
+        <p>값을 넘겨 받자(자식)</p>
+        <div>이름: {fromParent.name}</div>
+        <div>나이: {fromParent.age}</div>
+        <div>벨트: {fromParent.belt}</div>
+        <div>-----------------------</div>
+      </div>
+    )
+  })
 
-    return <div>{fromParentsList}</div>
-  }
+  return <div>{fromParentsList}</div>
 }
+
+export default child
